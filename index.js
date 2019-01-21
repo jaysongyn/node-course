831d40a86845cd360d734ace1bb9ced55cf5d831
jayson <jayson@5xmais.com.br>
2019-01-21 15:44:48 -0200

    test change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     debugger
-    res.send({ hi: 'there'})
+    res.send({ hi: 's'})
 })
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
