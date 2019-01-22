10ef1d47a07a755f190cad8559eff8e74e4ec924
jayson <jayson@5xmais.com.br>
2019-01-22 19:33:53 -0200

    change config files

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: rocess.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: rocess.env.MONGO_URI,
+    cookieKey: rocess.env.COOKIE_KEY
+}
