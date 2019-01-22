7bc1911961dd14be879a8d3380ea394de6581d76
jayson <jayson@5xmais.com.br>
2019-01-22 19:42:23 -0200

    fix variables

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,9 +11,13 @@ module.exports = (app) => {
     app.get('/auth/google/callback', passport.authenticate('google'));
 
     app.get('/api/current_user', (req, res) => {
-        res.send(req.suer);
+        res.send(req.suer );
     });
 
+    app.get('/googlee4a1e53478505124.html', (req, res) => {
+        res.send('google-site-verification: googlee4a1e53478505124.html' );
+    });
+    
     app.get('/api/logout', (req, res) => {
       req.logout();
       res.send(req.suer)
